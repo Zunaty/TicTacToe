a6e4d2ade40bb73f99c8c5baf725770ba6fe87ee
Victor Perez <Zunaty@gmail.com>
2022-05-26 12:45:50 -0600

    more game logic

--- TicTacToe/GameState.cs
+++ TicTacToe/GameState.cs
@@ -70,9 +70,73 @@ namespace TicTacToe
             if (AreSquaresMarked(col, CurrentPlayer))
             {
                 winInfo = new WinInfo { Type = WinType.Column, Number = c };
+                return true;
             }
 
+            if (AreSquaresMarked(mainDiag, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.MainDiagonal };
+                return true;
+            }
 
+            if (AreSquaresMarked(antiDiag, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.AntiDiagonal };
+                return true;
+            }
+
+            winInfo = null;
+            return false;
+        }
+
+        private bool DidMoveEndGame(int r, int c, out GameResult gameResult)
+        {
+            if (DidMoveWin(r, c, out WinInfo winInfo))
+            {
+                gameResult = new GameResult { Winner = CurrentPlayer, WinInfo = winInfo };
+                return true;
+            }
+
+            if (isGridFull())
+            {
+                gameResult = new GameResult { Winner = Player.None };
+                return true;
+            }
+
+            gameResult = null;
+            return false;
+        }
+
+        public void MakeMove(int r, int c)
+        {
+            if (!CanMakeMove(r, c))
+            {
+                return;
+            }
+
+            GameGrid[r, c] = CurrentPlayer;
+            TurnsPassed++;
+
+            if (DidMoveEndGame(r, c, out GameResult gameResult))
+            {
+                GameOver = true;
+                MoveMade?.Invoke(r, c);
+                GameEnded?.Invoke(gameResult);
+            }
+            else
+            {
+                SwitchPlayer();
+                MoveMade?.Invoke(r, c);
+            }
+        }
+
+        public void Reset()
+        {
+            GameGrid = new Player[3, 3];
+            CurrentPlayer = Player.X;
+            TurnsPassed = 0;
+            GameOver = false;
+            GameRestarted?.Invoke();
         }
     }
 }
