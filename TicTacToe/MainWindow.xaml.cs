eecc2eaf40947b5729504990f1492dc919046807
Victor Perez <Zunaty@gmail.com>
2022-06-05 14:20:34 -0600

    end screen

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<Player, ImageSource> imageSources = new()
+        {
+            { Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            { Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
+        };
+
+        private readonly Image[,] imageControls = new Image[3, 3];
+        private readonly GameState gameState = new GameState();
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
